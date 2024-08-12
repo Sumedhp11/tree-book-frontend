9443fa6be9a42e4e1dcacb2d6dc48b765ec6395a
Sumedh Pawar <sumedhpawar8966@gmail.com>
2024-08-13 02:51:12 +0530

    loader props bug fixed

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -4,7 +4,7 @@ import { Link, useLocation } from "react-router-dom";
 
 /* eslint-disable react/display-name */
 const Applayout = (WrappedComponent) => {
-  return ({ user }) => {
+  return ({ user, isLoading }) => {
     const location = useLocation();
     return (
       <div className="sm:h-screen h-dvh w-full grid grid-rows-12">
@@ -12,11 +12,11 @@ const Applayout = (WrappedComponent) => {
           <ul className="flex justify-center gap-4 items-center mr-5">
             <Link
               className={`text-base font-normal ${
-                location.pathname === "/"
+                location.pathname === "/add-tree"
                   ? "bg-zinc-300 text-black p-1 rounded"
                   : "text-white"
               }`}
-              to={"/"}
+              to={"/add-tree"}
             >
               Add Tree Form
             </Link>
@@ -33,7 +33,7 @@ const Applayout = (WrappedComponent) => {
           </ul>
         </div>
         <div className="row-span-11">
-          <WrappedComponent user={user} />
+          <WrappedComponent user={user} isLoading={isLoading} />
         </div>
       </div>
     );
